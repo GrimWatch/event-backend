f18866a4fcb2f296e54f6e8e176d7632b25a2c61
Vishnu Vardhan JS <jaisrini76@gmail.com>
2024-03-17 03:13:09 +0530

    [Added] Vercel config

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 
-dotenv.config({ path: "./config.env" });
+dotenv.config();
 
 const app = require('./app');
 const mongoose = require('mongoose');
 
-
+console.log(process.env.DATABASE_CONNECTION)
 mongoose.connect(process.env.DATABASE_CONNECTION)
     .then(() => console.log('Database is connected successfully'))
     .catch((err) => console.log(err))
