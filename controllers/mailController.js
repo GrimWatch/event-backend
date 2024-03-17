950799f690c9ddb0584ad5d56917e0229981e073
Vishnu Vardhan JS <jaisrini76@gmail.com>
2024-03-17 12:25:03 +0530

    [Testing] Post request for mail

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -24,7 +24,7 @@ exports.sendEmail = async (req, res) => {
           res.send("Email Sent")
 
     } catch (error) {
-        console.log(req.body)
+        res.send(req.body)
         res
             .status(400)
             .json({
